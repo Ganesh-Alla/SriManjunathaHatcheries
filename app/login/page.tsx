1dd5a93bf49cc46a2f377db17233c7407c5682cd
Ganesh Alla <allaganesh03@outlook.com>
2024-07-13 16:29:22 +0530

    login to home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from '@/utils/supabase/server';
 import { Metadata } from 'next';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -20,9 +21,9 @@ export default async function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-center rounded-lg bg-blue-500 p-3 md:h-36">
+        <Link href="/" className="flex h-20 w-full items-center rounded-lg bg-blue-500 p-3 md:h-36">
       <Image src="/smh-logo.svg" alt="alt" width={250} height={250} />
-        </div>
+        </Link>
         <LoginForm />
       </div>
     </main>
